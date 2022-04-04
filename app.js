e83075efd2073c0081b0a414ab7c9af6410f56eb
Andri Sutanto <me@andrisutanto.com>
2022-04-04 21:26:05 +0700

    EJS templating

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+//console.log(date());
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -12,15 +15,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  //ini manggil dari date.js
+  let day = date.getDate();
 
   res.render("list", {listTitle: day, newListItems: items});
 
